7950093b567fc455b2f0ffc95560636ba9f38382
GARO21225 <edgar.kouame@orange.com>
2026-08-17 23:14:01 +0000

    feat: add Flutter app scaffold (lib/main.dart)

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'theme/rezi_theme.dart';
+import 'screens/splash_screen.dart';
+
+void main() {
+  runApp(const ReziApp());
+}
+
+class ReziApp extends StatelessWidget {
+  const ReziApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'REZI',
+      debugShowCheckedModeBanner: false,
+      theme: ReziTheme.light,
+      darkTheme: ReziTheme.dark,
+      themeMode: ThemeMode.dark,
+      home: const SplashScreen(),
+    );
+  }
+}
